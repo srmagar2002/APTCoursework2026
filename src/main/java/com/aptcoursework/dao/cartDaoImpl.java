a7466b232918073090358e0c2eb479367bdb2ddc
Kushal Puri <kushalpuri@Kushals-MacBook-Air.local>
2026-04-15 20:38:32 +0545

    files added for testing

---
+++ src/main/java/com/aptcoursework/dao/cartDaoImpl.java
@@ -0,0 +1,13 @@
+package com.aptcoursework.dao;
+
+import com.aptcoursework.entity.Laptop;
+import com.aptcoursework.entity.User;
+
+public class cartDaoImpl {
+
+
+    public boolean addToCart(User user, Laptop laptop){
+
+        return false;
+    }
+}
