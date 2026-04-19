2ecf7a029f9d31fcf88b68367241e64b03d0a926
kushalpuri <purikkushal36@gmail.com>
2026-04-19 17:50:18 +0545

    place order completed

--- src/main/java/com/aptcoursework/dao/OrdersDaoImpl.java
+++ src/main/java/com/aptcoursework/dao/OrdersDaoImpl.java
@@ -2,16 +2,26 @@ package com.aptcoursework.dao;
 
 import com.aptcoursework.entity.Cart;
 import com.aptcoursework.entity.Orders;
+import com.aptcoursework.utils.DatabaseConnection;
 
 import java.math.BigDecimal;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class OrdersDaoImpl implements OrdersDao{
 
     cartDao cartdao = new cartDaoImpl();
     LaptopDao laptopdao = new LaptopDaoImpl();
+
+//    sql queries
+    String ordersSql = "INSERT INTO ORDERS (userId, totalAmount, status, estimatedDelivery) VALUES (?, ?, ?, ?)";
+    String orderItemsSql = "INSERT INTO order_items (orderId, laptopId, quantity) VALUES (?, ?, ?)";
+
     @Override
     public boolean placeOrder(int userId) {
 
@@ -24,6 +34,7 @@ public class OrdersDaoImpl implements OrdersDao{
             BigDecimal price = laptopdao.getLaptopById(cartItem.getLaptopId()).getPrice();
             BigDecimal quantity = new BigDecimal(cartItem.getQuantity());
             totalAmount.add(price.multiply(quantity));
+
         }
 
 
@@ -31,13 +42,42 @@ public class OrdersDaoImpl implements OrdersDao{
 
         try{
 
+//            Inserting into orders
+            conn = DatabaseConnection.getConnection();
+            PreparedStatement statement = conn.prepareStatement(ordersSql);
+            statement.setInt(1, userId);
+            statement.setBigDecimal(2, totalAmount);
+            statement.setString(3, "PREPARING");
 
-        }catch(Exception e){
+            LocalDate deliveryDate = LocalDate.now().plusDays(3);
+            statement.setDate(4, java.sql.Date.valueOf(deliveryDate));
+            statement.executeUpdate();
 
+//            get generated OrderId
+//            It is used to extract the currently generated id from sql.
+            ResultSet rs = statement.getGeneratedKeys();
+            int orderId = 0;
+            if(rs.next()){
+                orderId = rs.getInt(1);
+            }
+
+//            Inserting into order_items
+            PreparedStatement itemStmt = conn.prepareStatement(orderItemsSql);
+            for(Cart cartItem: cartItems){
+                itemStmt.setInt(1, orderId);
+                itemStmt.setInt(2, cartItem.getLaptopId());
+                itemStmt.setInt(3, cartItem.getQuantity());
+            }
+            itemStmt.executeUpdate();
+            return true;
+
+        }catch(SQLException e){
+            System.out.println("Error while placing order: "+e.getMessage());
+            return false;
+
+        }finally{
+            DatabaseConnection.closeConnection(conn);
         }
-
-
-        return false;
     }
 
     @Override
