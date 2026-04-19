24466a2ae70761404289039ac562078946b28d96
kushalpuri <purikkushal36@gmail.com>
2026-04-19 13:54:06 +0545

    Big decimal part

--- src/main/java/com/aptcoursework/dao/OrdersDaoImpl.java
+++ src/main/java/com/aptcoursework/dao/OrdersDaoImpl.java
@@ -1,12 +1,42 @@
 package com.aptcoursework.dao;
 
+import com.aptcoursework.entity.Cart;
 import com.aptcoursework.entity.Orders;
 
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class OrdersDaoImpl implements OrdersDao{
+
+    cartDao cartdao = new cartDaoImpl();
+    LaptopDao laptopdao = new LaptopDaoImpl();
     @Override
     public boolean placeOrder(int userId) {
+
+//        getting cart Items
+        ArrayList<Cart> cartItems = cartdao.fetchCartItems();
+
+//        getting total amount
+        BigDecimal totalAmount = new BigDecimal(0);
+        for(Cart cartItem: cartItems){
+            BigDecimal price = laptopdao.getLaptopById(cartItem.getLaptopId()).getPrice();
+            BigDecimal quantity = new BigDecimal(cartItem.getQuantity());
+            totalAmount.add(price.multiply(quantity));
+        }
+
+
+        Connection conn = null;
+
+        try{
+
+
+        }catch(Exception e){
+
+        }
+
+
         return false;
     }
 
