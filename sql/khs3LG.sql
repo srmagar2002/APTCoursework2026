e3a05de0761289e49222021f24e6f70663158b05
kushalpuri <purikkushal36@gmail.com>
2026-04-23 08:25:42 +0545

    sql edited

--- sql/khs3LG.sql
+++ sql/khs3LG.sql
@@ -6,6 +6,12 @@ KHS3LG;
 
 SET
 FOREIGN_KEY_CHECKS = 0;
+
+DROP TABLE IF EXISTS order_items;
+DROP TABLE IF EXISTS cart;
+DROP TABLE IF EXISTS orders;
+
+
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS laptop;
 SET
@@ -19,23 +25,22 @@ CREATE TABLE users
     password_hash VARCHAR(255) NOT NULL,
     role          VARCHAR(10)  NOT NULL,
     created_at    TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT role_const CHECK (role IN ('ADMIN', 'CUSTOMER'))
+    CONSTRAINT role_const CHECK (role IN ('ADMIN','CUSTOMER'))
 );
 
 CREATE TABLE laptop
 (
 /*BASIC INFO*/
     laptopID           INT AUTO_INCREMENT PRIMARY KEY,
-    laptopUUID         VARCHAR(300) NOT NULL UNIQUE,
     brand              VARCHAR(200)   NOT NULL,
     model              VARCHAR(200)   NOT NULL,
     title              TEXT           NOT NULL,
     description        TEXT,
     imgUrl             VARCHAR(255) GENERATED ALWAYS AS ( CONCAT('img/', CAST(laptopID AS CHAR), '.jpg')),
-    img1Url            VARCHAR(255) GENERATED ALWAYS AS ( CONCAT('img1/', CAST(laptopID AS CHAR), '.jpg')),
-    img2Url            VARCHAR(255) GENERATED ALWAYS AS ( CONCAT('img2/', CAST(laptopID AS CHAR), '.jpg')),
+    img1Url             VARCHAR(255) GENERATED ALWAYS AS ( CONCAT('img1/', CAST(laptopID AS CHAR), '.jpg')),
+    img2Url             VARCHAR(255) GENERATED ALWAYS AS ( CONCAT('img2/', CAST(laptopID AS CHAR), '.jpg')),
     thumbnailUrl       VARCHAR(255) GENERATED ALWAYS AS ( CONCAT('thumb/', CAST(laptopID AS CHAR), '.jpg')),
-    category           VARCHAR(200)            DEFAULT 'General',
+    `category`          VARCHAR(200)    DEFAULT 'General',
 
 /*SPECS*/
     processor          VARCHAR(200)   NOT NULL,
@@ -43,7 +48,7 @@ CREATE TABLE laptop
     storage            VARCHAR(200)   NOT NULL,
     storageType        VARCHAR(200)   NOT NULL,
     graphicsCard       VARCHAR(200)   NOT NULL,
-    screenSize         VARCHAR(200)   NOT NULL,
+    screenSize         DECIMAL(10, 2) NOT NULL,
     resolution         VARCHAR(200)   NOT NULL,
     operatingSystem    VARCHAR(200)   NOT NULL DEFAULT 'Windows',
 
@@ -67,13 +72,13 @@ CREATE TABLE laptop
 /*METADATA*/
     createdAt          TIMESTAMP               DEFAULT CURRENT_TIMESTAMP,
     updatedAt          TIMESTAMP               DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    CONSTRAINT categoryCheck CHECK (category IN
+    CONSTRAINT categoryCheck CHECK (`category` IN
                                     ('Gaming', 'Ultrabook', 'Business', 'Student', 'Convertible', 'Workstation',
-                                     'General', 'Professional')),
+                                     'General')),
     CONSTRAINT storageCheck CHECK (storageType IN ('SSD', 'HDD')),
-    CONSTRAINT osCheck CHECK (operatingSystem IN ('Windows', 'macOS', 'Chrome OS', 'Linux')),
+    CONSTRAINT osCheck CHECK (operatingSystem IN ('Windows', 'MacOS', 'Linux')),
     CONSTRAINT availabilityCheck CHECK (availabilityStatus IN ('IN STOCK', 'OUT OF STOCK'))
-)
+);
 /*
 Category Explanation
 Gaming -	High-performance for gaming
@@ -86,3 +91,35 @@ General	Default - general-purpose
   */
 
 
+CREATE TABLE cart (
+
+    cartId     INT PRIMARY KEY AUTO_INCREMENT,
+    userId     INT NOT NULL,
+    laptopId   INT NOT NULL,
+    quantity   INT NOT NULL DEFAULT 1,
+
+    CONSTRAINT fk_user_cart FOREIGN KEY (userId) REFERENCES users(user_id),
+    CONSTRAINT fk_laptop_cart FOREIGN KEY (laptopId) REFERENCES laptop(laptopID)
+
+);
+
+CREATE TABLE orders (
+    orderId INT AUTO_INCREMENT PRIMARY KEY,
+    userId INT,
+    totalAmount DECIMAL(10, 2) NOT NULL,
+    status VARCHAR(20),
+    estimatedDelivery DATE,
+    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT fk_user_orders FOREIGN KEY (userId) REFERENCES users(user_id)
+);
+
+CREATE TABLE order_items (
+     orderItemId INT AUTO_INCREMENT PRIMARY KEY,
+     orderId INT,
+     laptopId INT,
+     quantity INT,
+
+     CONSTRAINT fk_order_items FOREIGN KEY (orderId) REFERENCES orders(orderId),
+     CONSTRAINT fk_laptop_items FOREIGN KEY (laptopId) REFERENCES laptop(laptopID)
+);
