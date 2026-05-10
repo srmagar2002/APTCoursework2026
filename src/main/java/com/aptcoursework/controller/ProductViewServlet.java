65433d4a2d48b4ad07a02904259b4baace7efe69
srmagar2002 <167953839+srmagar2002@users.noreply.github.com>
2026-05-10 12:51:54 +0545

    Minor changes

--- src/main/java/com/aptcoursework/controller/ProductViewServlet.java
+++ src/main/java/com/aptcoursework/controller/ProductViewServlet.java
@@ -12,6 +12,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 
 /**
@@ -25,6 +26,7 @@ import java.util.ArrayList;
 public class ProductViewServlet extends HttpServlet {
 
     private static final LaptopDaoImpl laptopDao = new LaptopDaoImpl();
+
     /**
      * Handles GET requests by retrieving and displaying detailed product information.
      * Fetches laptop details by ID and forwards to the product view page.
@@ -32,17 +34,30 @@ public class ProductViewServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String action = request.getParameter("action");
-        int productId = Integer.parseInt(request.getParameter("laptopID"));
+        int laptopID = Integer.parseInt(request.getParameter("laptopID"));
 
-        Laptop laptop = laptopDao.getLaptopById(productId);
+        Laptop laptop = laptopDao.getLaptopById(laptopID);
         System.out.println(laptop.toString());
+        request.setAttribute("laptop", laptop);
 
-            request.setAttribute("laptop", laptop);
-            request.getRequestDispatcher("/WEB-INF/views/pages/productViewPage.jsp")
-                    .forward(request, response);
+
+        RatingDaoImpl ratingDao = new RatingDaoImpl();
+
+        double avgRating = ratingDao.getAvgRatingbyLaptop(laptopID);
+        DecimalFormat df = new DecimalFormat("#.#");
+        request.setAttribute("productAvgRating", df.format(avgRating));
+        request.setAttribute("productAvgRatingRounded", (int) avgRating);
+
+        int totalRating = ratingDao.getRatingsByLaptop(laptopID).size();
+        request.setAttribute("productTotalRating", totalRating);
+
+
+        request.getRequestDispatcher("/WEB-INF/views/pages/productViewPage.jsp")
+                .forward(request, response);
 
 
     }
+
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
